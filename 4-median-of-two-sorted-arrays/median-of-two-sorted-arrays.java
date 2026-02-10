5ceaa22c71b07658580681245f13361cea69c5df
Partho Paul <10legendgaming@gmail.com>
2026-02-10 16:18:39 +0600

    Time: 7 ms (25.97%) | Memory: 49.3 MB (13.84%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -21,12 +21,5 @@ class Solution {
         }
         
     }
-    static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-    try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
-}
+    
 }
